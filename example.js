005fb520c306b750add0d16419e31b76ed3a6547
Eduard Kleiner <ediklainer@gmail.com>
2021-11-21 21:30:55 +0200

    Added HashPower class + getMyOrders method

--- example.js
+++ example.js
@@ -1,10 +1,15 @@
 const fs = require('fs');
-const Api = require('nicehash-api-wrapper-v2');
+//const Api = require('nicehash-api-wrapper-v2');
+const Api = require("./api"); // local files
 
-var api = new Api(JSON.parse(fs.readFileSync('secret.json')));
-//var api = new Api({});
+const SECRET = JSON.parse(fs.readFileSync('secret.json'));
 
-api.MinerPrivate.getMiningAddress().then((res) => {
+var api = new Api(SECRET);
+
+/* api.MinerPrivate.getMiningAddress().then((res) => {
+    console.log(res);
+}); */
+
+api.HashPower.getMyOrders().then((res) => {
     console.log(res);
 });
-
